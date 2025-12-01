a7360f024f1c9a71e04ed632f22bbefa4aaf2eaa
Sam Cunningham <cunningham.s@epicompliance.com>
2025-12-01 13:20:55 -0500

    vite server updates for prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
-    port: 5175
-  }
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5175,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+  },
 })
